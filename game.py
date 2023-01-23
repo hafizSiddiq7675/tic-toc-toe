2f715ab05ac97e8217b910add58a7b2270b97a09
hafizSiddiq7675 <hafiz.siddiq.umer@gmail.com>
2023-01-23 10:58:49 +0500

    Update Variable Names

--- game.py
+++ game.py
@@ -10,8 +10,8 @@ c2 = " "
 c3 = " "
 
 # Input from User as "inNum" and a "winCheck" variable for Win or Draw check. 
-inNum = 0
-winCheck = 0
+in_number = 0
+win_check = 0
 
 # Two customized function first to print the Grid and second to check 
 # If the box selectes by player is legitimate or not.
@@ -47,43 +47,43 @@ def printBoard():
 	print("|")
 
 	print("+ - + - + - +")
-def authenticate(inNum):
-	if inNum > 9 or inNum < 1:
+def authenticate(in_number):
+	if in_number > 9 or in_number < 1:
 		print("Please Enter valid Number between 1 - 9")
 		return 0
-	if inNum == 1:
+	if in_number == 1:
 		if a1 != " ":
 			print("Please enter another number. This box is already taken")
 			return 0
-	elif inNum == 2:
+	elif in_number == 2:
 		if a2 != " ":
 			print("Please enter another number. This box is already taken")
 			return 0
-	elif inNum == 3:
+	elif in_number == 3:
 		if a3 != " ":
 			print("Please enter another number. This box is already taken")
 			return 0
-	elif inNum == 4:
+	elif in_number == 4:
 		if b1 != " ":
 			print("Please enter another number. This box is already taken")
 			return 0
-	elif inNum == 5:
+	elif in_number == 5:
 		if b2 != " ":
 			print("Please enter another number. This box is already taken")
 			return 0
-	elif inNum == 6:
+	elif in_number == 6:
 		if b3 != " ":
 			print("Please enter another number. This box is already taken")
 			return 0
-	elif inNum == 7:
+	elif in_number == 7:
 		if c1 != " ":
 			print("Please enter another number. This box is already taken")
 			return 0
-	elif inNum == 8:
+	elif in_number == 8:
 		if c2 != " ":
 			print("Please enter another number. This box is already taken")
 			return 0
-	elif inNum == 9:
+	elif in_number == 9:
 		if c3 != " ":
 			print("Please enter another number. This box is already taken")
 			return 0
@@ -99,138 +99,138 @@ for x in range(9):
 	# This is if condition to alternate players turn by using even odd logic.
 	if (x % 2) == 0:
 		print("Player 1 Turn. Please select your Box........")
-		inNum = int(input("Enter a number: "))
+		in_number = int(input("Enter a number: "))
 		#This while is to get authentic input from the player. It will run until legitimate entry is entered.
-		while authenticate(inNum) == 0:
-			inNum = int(input("Enter a number: "))
+		while authenticate(in_number) == 0:
+			in_number = int(input("Enter a number: "))
 		#This if condition to store and update the Board.
-		if inNum == 1:
+		if in_number == 1:
 			a1 = "O"
 			printBoard()
-		elif inNum == 2:
+		elif in_number == 2:
 			a2 = "O"
 			printBoard()
-		elif inNum == 3:
+		elif in_number == 3:
 			a3 = "O"
 			printBoard()
-		elif inNum == 4:
+		elif in_number == 4:
 			b1 = "O"
 			printBoard()
-		elif inNum == 5:
+		elif in_number == 5:
 			b2 = "O"
 			printBoard()
-		elif inNum == 6:
+		elif in_number == 6:
 			b3 = "O"
 			printBoard()
-		elif inNum == 7:
+		elif in_number == 7:
 			c1 = "O"
 			printBoard()
-		elif inNum == 8:
+		elif in_number == 8:
 			c2 = "O"
 			printBoard()
-		elif inNum == 9:
+		elif in_number == 9:
 			c3 = "O"
 			printBoard()
 	else:
 		print("Player 2 Turn. Please select your Box........")
-		inNum = int(input("Enter a number: "))
+		in_number = int(input("Enter a number: "))
 		#This while is to get authentic input from the player. It will run until legitimate entry is entered.
-		while authenticate(inNum) == 0:
-			inNum = int(input("Enter a number: "))
+		while authenticate(in_number) == 0:
+			in_number = int(input("Enter a number: "))
 		#This if condition to store and update the Board.
-		if inNum == 1:
+		if in_number == 1:
 			a1 = "X"
 			printBoard()
-		elif inNum == 2:
+		elif in_number == 2:
 			a2 = "X"
 			printBoard()
-		elif inNum == 3:
+		elif in_number == 3:
 			a3 = "X"
 			printBoard()
-		elif inNum == 4:
+		elif in_number == 4:
 			b1 = "X"
 			printBoard()
-		elif inNum == 5:
+		elif in_number == 5:
 			b2 = "X"
 			printBoard()
-		elif inNum == 6:
+		elif in_number == 6:
 			b3 = "X"
 			printBoard()
-		elif inNum == 7:
+		elif in_number == 7:
 			c1 = "X"
 			printBoard()
-		elif inNum == 8:
+		elif in_number == 8:
 			c2 = "X"
 			printBoard()
-		elif inNum == 9:
+		elif in_number == 9:
 			c3 = "X"
 			printBoard()
 
 	#This is logical if condition to check if Player 1 has Won.
 	if a1 == "O" and a2 == "O" and a3 == "O":
-		winCheck = 1
+		win_check = 1
 		print("Player 1 Wins the Game")
 		break
 	elif b1 == "O" and b2 == "O" and b3 == "O":
-		winCheck = 1
+		win_check = 1
 		print("Player 1 Wins the Game")
 		break
 	elif c1 == "O" and c2 == "O" and c3 == "O":
-		winCheck = 1
+		win_check = 1
 		print("Player 1 Wins the Game")
 		break
 	elif a1 == "O" and b1 == "O" and c1 == "O":
-		winCheck = 1
+		win_check = 1
 		print("Player 1 Wins the Game")
 		break
 	elif a2 == "O" and b2 == "O" and c2 == "O":
-		winCheck = 1
+		win_check = 1
 		print("Player 1 Wins the Game")
 		break
 	elif a3 == "O" and b3 == "O" and c3 == "O":
-		winCheck = 1
+		win_check = 1
 		print("Player 1 Wins the Game")
 		break
 	elif a1 == "O" and b2 == "O" and c3 == "O":
-		winCheck = 1
+		win_check = 1
 		print("Player 1 Wins the Game")
 		break
 	elif a3 == "O" and b2 == "O" and c1 == "O":
-		winCheck = 1
+		win_check = 1
 		print("Player 1 Wins the Game")
 		break
 
 	#This is logical if condition to check if Player 2 has Won.
 	if a1 == "X" and a2 == "X" and a3 == "X":
-		winCheck = 1
+		win_check = 1
 		print("Player 2 Wins the Game")
 		break
 	elif b1 == "X" and b2 == "X" and b3 == "X":
-		winCheck = 1
+		win_check = 1
 		print("Player 2 Wins the Game")
 		break
 	elif c1 == "X" and c2 == "X" and c3 == "X":
-		winCheck = 1
+		win_check = 1
 		print("Player 2 Wins the Game")
 		break
 	elif a1 == "X" and b1 == "X" and c1 == "X":
-		winCheck = 1
+		win_check = 1
 		print("Player 2 Wins the Game")
 		break
 	elif a2 == "X" and b2 == "X" and c2 == "X":
-		winCheck = 1
+		win_check = 1
 		print("Player 2 Wins the Game")
 		break
 	elif a3 == "X" and b3 == "X" and c3 == "X":
-		winCheck = 1
+		win_check = 1
 		print("Player 2 Wins the Game")
 		break
 	elif a1 == "X" and b2 == "X" and c3 == "X":
-		winCheck = 1
+		win_check = 1
 		print("Player 2 Wins the Game")
 		break
 	elif a3 == "X" and b2 == "X" and c1 == "X":
-		winCheck = 1
+		win_check = 1
 		print("Player 2 Wins the Game")
 		break
 
@@ -238,6 +238,6 @@ for x in range(9):
 
 
 # This is to declare draw only if nobody won.
-if winCheck == 0:
-	print("Gane Draw!")
+if win_check == 0:
+	print("Game Draw!")
 
